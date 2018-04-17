f95ba9e8f47ce492839919d7187e642eb29503a0
Stephen Dodd <Stephen.Dodd@resmed.com>
2018-04-17 23:17:16 +0100

    Commiting first pass at solution + Testbench code

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  xInv <- NULL
+  set <- function(y) {
+    xInv <<- y
+    xInv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(solve) xInv <<- solve
+  getInv <- function() xInv
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInv(m)
+  m
 }
